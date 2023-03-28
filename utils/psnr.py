798f88fb8583cfda060236d759227ea15efb49d5
Ano Ndachena <anotidaishendachena@gmail.com>
2023-03-28 11:12:14 -0700

    remove cv2 dep

--- utils/psnr.py
+++ utils/psnr.py
@@ -1,5 +1,5 @@
 from math import log10, sqrt
-import cv2
+# import cv2
 import numpy as np
   
 def PSNR(original, compressed):
@@ -11,13 +11,13 @@ def PSNR(original, compressed):
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
   
-def main():
-     original = cv2.imread("fmen_python.png")
-     #compressed = cv2.imread("C:\\Users\\Junbin\\Downloads\\Images for PSNR check\\frame0060_HAT_SRx2.png", 1)
-     #compressed = cv2.imread("C:\\Users\\Junbin\\Downloads\\Images for PSNR check\\frame0060_RLFN_SRx2.png", 1)
-     compressed = cv2.imread("onnx_runtime.png", 1)
-     value = PSNR(original, compressed)
-     print(f"PSNR value is {value} dB")
+# def main():
+#      original = cv2.imread("fmen_python.png")
+#      #compressed = cv2.imread("C:\\Users\\Junbin\\Downloads\\Images for PSNR check\\frame0060_HAT_SRx2.png", 1)
+#      #compressed = cv2.imread("C:\\Users\\Junbin\\Downloads\\Images for PSNR check\\frame0060_RLFN_SRx2.png", 1)
+#      compressed = cv2.imread("onnx_runtime.png", 1)
+#      value = PSNR(original, compressed)
+#      print(f"PSNR value is {value} dB")
        
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
